c32449da2147b96beda2a07b3d8bf5c6e2c23c7e
XandraGates <59373228+XandraGates@users.noreply.github.com>
2020-02-23 22:07:53 +0000

    HTML, CSS and JS files

---
+++ index.js
@@ -0,0 +1,27 @@
+var fname = prompt("Please insert your first name");
+prompt("Please insert your last name");
+prompt("Please insert your age");
+var luckyNumber = prompt("Please insert your lucky number");
+prompt("If you win, what would you spend the money on?");
+
+if(isNumeric(luckyNumber)){
+  if(luckyNumber == 2){
+    document.getElementById("lotteryresult").src="images/lottery2.PNG";
+  }else if(luckyNumber == 10){
+    document.getElementById("lotteryresult").src="images/lottery10.PNG";
+  }else if(luckyNumber == 15){
+    document.getElementById("lotteryresult").src="images/lottery15.PNG";
+  }else if(luckyNumber == 17){
+    document.getElementById("lotteryresult").src="images/lottery17.PNG";
+  }else{
+    document.getElementById("lotteryresult").src="images/lottery_lost.PNG";
+    document.getElementById("wrongNumb").innerHTML= luckyNumber;
+  }
+  document.getElementById("thankU").innerHTML=`Thank you for playing, ${fname}`;
+}else{
+  alert("Not valid - please refresh the page and pick a number");
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
